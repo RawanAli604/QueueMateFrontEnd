962e226cd72586768458efa728e606a41365dae4
RawanAli <rawan.ali.fardan@email.com>
2025-12-26 14:56:56 +0300

    updated nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,12 @@
 import { useContext } from 'react';
-import { Link } from 'react-router';
+import { Link, NavLink } from 'react-router-dom';
 import './NavBar.css';
 import { UserContext } from '../../contexts/UserContext';
-import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
-
   const { user, setUser } = useContext(UserContext);
 
-   const handleSignOut = () => {
+  const handleSignOut = () => {
     localStorage.removeItem('token');
     setUser(null);
   };
@@ -19,25 +17,49 @@ const NavBar = () => {
         <div className='nav-logo'>
           QueueMate
         </div>
-      {user ? (
-        <ul className='nav-links'>
-          <li className='nav-link'><Link to='/'>Dashboard</Link></li>
-          <li className='nav-link'><Link to='/venues'>Browse Venues</Link></li>
-          <li className='nav-link'><Link to='/waitlist'>History</Link></li>
-          <li className='nav-link'><Link to='/' onClick={handleSignOut}>Sign Out</Link></li>
-        </ul>
-      ) : (
-        <ul>
-          <li className='nav-link'><Link to='/'>Home</Link></li>
-          <li className='nav-link'><NavLink to="/about">About</NavLink></li>
-          <li className='nav-link'><Link to='/sign-up'>Sign Up</Link></li>
-          <li className='nav-link'><Link to='/sign-in'>Sign In</Link></li>
-        </ul>
-      )}
+
+        {user ? (
+          <ul className='nav-links'>
+
+            {/* Links for Customer */}
+            {user.role === 'customer' && (
+              <>
+                <li className='nav-link'><Link to='/'>Dashboard</Link></li>
+                <li className='nav-link'><Link to='/waitlist'>My Waitlist</Link></li>
+                <li className='nav-link'><Link to='/venues'>Venues</Link></li>
+              </>
+            )}
+
+            {user.role === 'staff' && (
+              <>
+                <li className='nav-link'><Link to='/'>Dashboard</Link></li>
+                <li className='nav-link'><Link to='/venues'>Manage Venues</Link></li>
+                <li className='nav-link'><Link to='/create/venue'>Create Venues</Link></li>
+              </>
+            )}
+
+            {user.role === 'admin' && (
+              <>
+                <li className='nav-link'><Link to='/'>Admin Dashboard</Link></li>
+                <li className='nav-link'><Link to='/admin/staff'>Add Staff</Link></li>
+                <li className='nav-link'><Link to='/admin/users'>Manage Users</Link></li>
+                <li className='nav-link'><Link to='/analytics'>Analytics</Link></li>
+              </>
+            )}
+
+            <li className='nav-link'><Link to='/' onClick={handleSignOut}>Sign Out</Link></li>
+          </ul>
+        ) : (
+          <ul className='nav-links'>
+            <li className='nav-link'><Link to='/'>Home</Link></li>
+            <li className='nav-link'><NavLink to='/about'>About</NavLink></li>
+            <li className='nav-link'><Link to='/sign-up'>Sign Up</Link></li>
+            <li className='nav-link'><Link to='/sign-in'>Sign In</Link></li>
+          </ul>
+        )}
       </div>
     </nav>
   );
 };
 
 export default NavBar;
-
